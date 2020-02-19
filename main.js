932481ddc6ce75b3c8edb70229d94e8afa1171e3
Michael Passade <michael.passade@codeimmersives.com>
2020-02-19 14:10:42 -0500

    done

--- main.js
+++ main.js
@@ -1,25 +1,97 @@
-function crazyCase() {
+function crazyCase(str) {
+  let x = ''
+  for (i=0; i<str.length; i++) {
+    i % 2 === 0 ? x=x+str[i].toLowerCase() : x=x+str[i].toUpperCase()
+  }
+  return x
 }
 
-function ciEmailify() {
+function ciEmailify(str) {
+  let x = str.substring(0,str.indexOf(' '))
+  let y = str.substring(str.indexOf(' ')+1)
+  str = (x+'.'+y+'@codeimmersives.com').toLowerCase()
+  return str
 }
 
-function exclaim() {
+function exclaim(str) {
+  let x = ''
+  for (i=0; i<str.length; i++){
+    if (str[i] === '.' || str[i] === '?'){
+      x += '!'
+    }
+    else {
+      x += str[i]
+    }
+  }
+  return x
 }
 
-function reverse() {
+function reverse(str) {
+  let x = ''
+  for (i=str.length-1; i >= 0; i--){
+    x += str[i]
+  }
+  return x
 }
 
-function crazyCase2ReturnOfCrazyCase() {
+function crazyCase2ReturnOfCrazyCase(str){
+  let x = ''
+  for (i=0, y=0; i<str.length; i++, y++){
+    if (str[i-1] === ' '){
+      y++
+    }
+    if (y % 2 === 0){
+      x += str[i].toLowerCase()
+    }
+    else {
+      x += str[i].toUpperCase()
+    }
+  }
+  return x
 }
 
-function titleCase() {
+function titleCase(str) {
+  let x = ''
+  for (i=0; i < str.length; i++){
+    if (i=== 0){
+      x += str[i].toUpperCase()
+    }
+    else if (str[i-1] === ' '){
+      x += str[i].toUpperCase()
+    }
+    else {
+      x += str[i].toLowerCase()
+    }
+  }
+  return x
 }
 
-function onlyVowels() {
+function onlyVowels(str) {
+  let x = ''
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  for (i=0; i<str.length; i++){
+    if (vowels.includes(str[i])){
+      x += str[i]
+    }
+  }
+return x
 }
 
-function crazyCase3SonOfCrazyCase() {
+function crazyCase3SonOfCrazyCase(str) {
+  let x = ''
+  let symbols = ' `1234567890-=[]\;,./~!@#$%^&*()_+{}|:"<>?'
+  for (i=0, y=0; i<str.length; i++, y++){
+    if (symbols.includes(str[i-1])){
+      y++
+    }
+    if (y % 2 === 0){
+      x += str[i].toLowerCase()
+    }
+    else {
+      x += str[i].toUpperCase()
+    }
+  }
+  return x
 }
 
 
